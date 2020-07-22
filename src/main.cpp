2bd7cf0c8f6c82348f197caf5613697433eddb9b
George Florea Bănuș <georgefb899@gmail.com>
2020-07-22 22:56:36 +0300

    Version 0.1.4

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
     KAboutData aboutData(
                 QStringLiteral("haruna"),
                 i18n("Haruna Video Player"),
-                QStringLiteral("0.1.3"),
+                QStringLiteral("0.1.4"),
                 i18n("A simple video player."),
                 KAboutLicense::GPL_V3,
                 i18n("(c) 2019"),
