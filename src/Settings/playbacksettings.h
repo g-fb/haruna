c7a988db8740e999244d5b52786989911f21580e
George Florea Bănuș <georgefb899@gmail.com>
2020-10-03 22:10:11 +0300

    Add license headers to settings classes

--- src/Settings/playbacksettings.h
+++ src/Settings/playbacksettings.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2020 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef PLAYBACKSETTINGS_H
 #define PLAYBACKSETTINGS_H
 
